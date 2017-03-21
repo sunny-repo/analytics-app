3ea795e8a6fde3d01103fe34b62c8235e9a1185e
Sunny Piya <sunnypiya@Sunnys-MBP.local>
2017-03-20 21:23:43 -0400

    adding new funcion for DNA interation coefficient calcualtion

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ element = 5
 version = 1.2
 topic = "Protein prediction module"
 Title = "Github init"
+
 def protein_struc(x):
 	pass
+
+def DNA_protein_interaction_coff(m,n):
+	pass
+
 print("Executing the first script in the series!")
